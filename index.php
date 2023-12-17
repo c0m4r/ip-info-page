2aa39093b3736eeba5f05db07963b67179ec03c2
c0m4r <github@wolfet.pl>
2023-12-17 23:27:12 +0100

    Update index.php
    curl/wget support, additional validations

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 
-require_once 'vendor/autoload.php'; // composer
-use GeoIp2\Database\Reader; // geoip
+// IP Info Page
+// https://github.com/c0m4r/ip-info-page
+
+require_once 'vendor/autoload.php'; // Composer
+use GeoIp2\Database\Reader; // GeoIP2-php
 
 $ua = $_SERVER['HTTP_USER_AGENT'];
 
+// Get CloudFlare CF_CONNECTING_IP if present
 if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
     $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-    $cfcountry = $_SERVER['HTTP_CF_IPCOUNTRY'];
-}
-else {
+} else {
     $ip = $_SERVER['REMOTE_ADDR'];
-    $cfcountry = '';
 }
 
+// Load GeoIP2-php database
 // https://dev.maxmind.com/geoip/geolite2-free-geolocation-data
 try {
     $cityDbReader = new Reader('GeoLite2-City_20231215/GeoLite2-City.mmdb');
@@ -22,14 +24,39 @@ try {
     $geoip = array();
 }
 
-// Twig
-$loader = new \Twig\Loader\FilesystemLoader('tpl');
-$twig = new \Twig\Environment($loader);
+// Print IP info
+if(preg_match('/^(curl|wget)/i', $_SERVER['HTTP_USER_AGENT'])) {
+    // JSON (curl/wget only)
+    header('Content-Type: application/json; charset=utf-8');
 
-echo $twig->render('index.html', [
-    'ip' => $ip,
-    'ua' => $ua,
-    'geoip' => $geoip
-]);
+    if(is_object($geoip))
+    {
+        $data = array(
+            'ip' => $ip,
+            'geoip' => true,
+            'country' => $geoip->country->name,
+            'region' => $geoip->mostSpecificSubdivision->name,
+            'city' => $geoip->city->name,
+            'postal' => $geoip->postal->code,
+            'ua' => $ua
+        );
+    } else
+    {
+        $data = array('ip' => $ip, 'ua' => $ua, 'geoip' => NULL);
+    }
+
+    echo json_encode($data, JSON_PRETTY_PRINT);
+} else
+{
+    // Twig
+    $loader = new \Twig\Loader\FilesystemLoader('tpl');
+    $twig = new \Twig\Environment($loader);
+
+    echo $twig->render('index.html', [
+        'ip' => $ip,
+        'ua' => $ua,
+        'geoip' => false
+    ]);
+}
 
 ?>
