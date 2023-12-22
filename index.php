5a357b6df32f6d1d23accc50d4d982e1ed99bc4a
c0m4r <github@wolfet.pl>
2023-12-22 20:53:18 +0100

    Update index.php
    Validations and error handling added

--- index.php
+++ index.php
@@ -1,36 +1,51 @@
 <?php
 
-// IP Info Page
+// IP info page
 // https://github.com/c0m4r/ip-info-page
 
-require_once 'vendor/autoload.php'; // Composer
+if(!empty($_GET) or !empty($_POST)) {
+    header('HTTP/1.0 405 Method Not Allowed'); exit("405 Method Not Allowed\n");
+}
+
+if(is_file('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php'; // Composer
+} else {
+    exit("Composer not installed\n");
+}
+
 use GeoIp2\Database\Reader; // GeoIP2-php
 
-$ua = $_SERVER['HTTP_USER_AGENT'];
+if(isset($_SERVER['HTTP_USER_AGENT'])) {
+    $ua = htmlspecialchars($_SERVER['HTTP_USER_AGENT']);
+} else {
+    $ua = NULL;
+}
 
 // Get CloudFlare CF_CONNECTING_IP if present
-if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+if(isset($_SERVER['HTTP_CF_CONNECTING_IP']) and filter_var($_SERVER['HTTP_CF_CONNECTING_IP'], FILTER_VALIDATE_IP)) {
     $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-} else {
+} elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']) and filter_var($_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP)) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} elseif(isset($_SERVER['REMOTE_ADDR']) and filter_var($_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP)) {
     $ip = $_SERVER['REMOTE_ADDR'];
+} else {
+    header('HTTP/1.0 501 Not Implemented'); exit("501 Not Implemented\n");
 }
 
 // Load GeoIP2-php database
 // https://dev.maxmind.com/geoip/geolite2-free-geolocation-data
 try {
-    $cityDbReader = new Reader('/path/to/GeoLite2-City.mmdb');
+    $cityDbReader = new Reader('.geolite2/GeoLite2-City.mmdb');
     $geoip = $cityDbReader->city($ip);
 } catch(Exception $e) {
     $geoip = array();
 }
 
 // Print IP info
-if(preg_match('/^(curl|wget)/i', $_SERVER['HTTP_USER_AGENT'])) {
-    // JSON (curl/wget only)
+if(preg_match('/^(curl|wget)/i', $ua)) {
     header('Content-Type: application/json; charset=utf-8');
 
-    if(is_object($geoip))
-    {
+    if(is_object($geoip)) {
         $data = array(
             'ip' => $ip,
             'geoip' => true,
@@ -40,8 +55,7 @@ if(preg_match('/^(curl|wget)/i', $_SERVER['HTTP_USER_AGENT'])) {
             'postal' => $geoip->postal->code,
             'ua' => $ua
         );
-    } else
-    {
+    } else {
         $data = array('ip' => $ip, 'ua' => $ua, 'geoip' => false);
     }
 
