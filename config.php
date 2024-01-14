c22fd76c90c9ac8ac647165dd8700c6484a6aabe
c0m4r <github@wolfet.pl>
2024-01-14 09:38:53 +0100

    path to GeoLite2-City.mmdb database

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 $config = (object) array
 (
+    "geoip_db" => 'GeoLite2-City.mmdb', // path to GeoLite2-City.mmdb database
     "csp" => true, // adds a Content-Security-Policy (CSP) security header with random nonce
     "sri" => true // adds a Subresource Integrity (SRI) hash for css/js integrity checks
 );
